ea37e73ab5a7e6b10ca1f586277e05446fd64dcc
sh6986 <okok960411@gmail.com>
2021-12-17 19:20:34 +0900

    express-session 미들웨어

--- lecture/ch6/app.js
+++ lecture/ch6/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);  // 서버에다 변수?속성을 심는 느낌. 어디에서든 쓸수있는 전역변수같은
@@ -36,8 +37,31 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 // 라우터로 넘어가서 /about 주소를 매핑한다.
 // 혹은 정적파일을 요청한건데 그앞에 다른 cookieParser등의 필요없는 미들웨어가 앞에 있을경우 비효율적이므로 이곳에 위치하는게 적합
 
+
+// 해당 요청에만 정적파일을 보내주는 경우 (로그인 한 사람한테만 사진을 보여주고 싶다.)
+// 미들웨어 확장법
+app.use('/', (req, res, next) => {
+    if (req.session.id) {
+        express.static(path.join(__dirname, 'public'))(req, res, next);
+    } else {
+        next();
+    }
+});
+
+
 app.use(cookieParser());
 // app.use(cookieParser('zerochopassword'));  // 암호화된 쿠키
+
+app.use(session());     // const session = {}; 처럼 세션객체 하나가 생긴다고 생각하면 됨
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: 'zerochopassword',
+//     cookie: {
+//         httpOnly: true,     // 자바스크립트로 공격당하지 않기 위해
+//     },
+//     name: 'connect.sid'
+// }));
 
 // 예전에는 body-parser를 require해서 사용했지만 지금은 body-parser가 express안에 들어가서
 // require하지 않고 아래처럼 사용하면 된다.
@@ -46,11 +70,19 @@ app.use(express.json());        // 클라이언트가 json으로 보냈을 때 j
 app.use(express.urlencoded({ extended: true }));    // 클라이언트에서 form 보낼때 파싱해서 body로 넣어줌
         // extended true면 qs 모듈을 사용하고 false면 querystring모듈을 사용하는데 qs 모듈이 훨씬 강력하므로 왠만하면 true
 
-
 /**
  * 라우터
  */
+app.use((req, res, next) => {
+    // 다음 라우터나 미들웨어로 값을 보내고 싶을때 (전역변수나 app.set() 절대x. 요청마다 공유되기 때문에 값이 달라질 수 있다. 공유되도 상관없는 데이터는 가능)
+    req.session.data = 'zerocho비번';     // 방법1. 단점은 세션에 저장되있으므로 다음 요청때도 값이 남아있다. ex)로그인하면 계속 요청할때마다 아이디 전달하는경우
+    req.data = 'zerocho비번';             // 방법2. 요청이 끝나면 메모리에서 정리되므로 안전하게 사용가능
+});
+
 app.get('/', (req, res) => {
+    req.session.data    // zerocho 비번. 다른 미들웨어와 값 공유하기 방법1.
+    req.data            // zerocho 비번. 다른 미들웨어와 값 공유하기 방법2.
+
     req.cookies     // { mycookie: 'test' }     // 쿠키 가져오기
     // req.signedCookies;  // 암호화된 쿠키 가져올때
     res.cookie('name', encodeURIComponent(name), {      // 쿠키 담기
@@ -64,6 +96,10 @@ app.get('/', (req, res) => {
     });
 
     // req.body.name // body 데이터 가져올때
+
+    res.session 		// 그 사용자의 고유한 세션을 가져올 수 있음
+    res.session.id = 'hello';     // 모든 사용자의 id값이 hello 가 되는것이 아니라 그 사용자의 id 만 hello 가 됨.
+                                    // 개인의 저장공간이 하나 생긴 느낌
 
     res.sendFile(path.join(__dirname, './index.html'));
     // res.send('안녕하세요');
