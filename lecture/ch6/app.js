a0fb5e6bed62cf39a7d2409f7457fc0e6f8979f3
sh6986 <okok960411@gmail.com>
2021-12-17 11:18:45 +0900

    미들웨어 사용하기

--- lecture/ch6/app.js
+++ lecture/ch6/app.js
@@ -4,12 +4,21 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);  // 서버에다 변수?속성을 심는 느낌. 어디에서든 쓸수있는 전역변수같은
 
+app.use((req, res, next) => {
+    console.log('모든 요청에 실행');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.post('/', (req, res) => {
     res.send('hello express');
+});
+
+app.get('/category/:name', (req, res) => {
+    res.send(`hello ${req.params.name}`);
 });
 
 app.get('/about', (req, res) => {
