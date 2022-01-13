c281e90a8baac6436ab2b07111e8825b67deb925
sh6986 <okok960411@gmail.com>
2022-01-13 15:40:26 +0900

    시퀄라이즈 실습하기

--- lecture/ch7/routes/comments.js
+++ lecture/ch7/routes/comments.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res, next) => {
     try {
         const comment = await Comment.create({
             commenter: req.body.id,
-            comment: req.body.createComment,
+            comment: req.body.comment,
         });
         console.log(comment);
         res.status(201).json(comment);
