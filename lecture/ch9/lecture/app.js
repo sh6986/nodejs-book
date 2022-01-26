ea9b70aa049a9126aa0a5e920e2e8755954d6b42
sh6986 <okok960411@gmail.com>
2022-01-26 20:29:21 +0900

    multer 사용하기

--- lecture/ch9/lecture/app.js
+++ lecture/ch9/lecture/app.js
@@ -13,6 +13,7 @@ const passport = require('passport');
 dotenv.config();
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
 const {sequelize} = require('./models');
 const passportConfig = require('./passport');   // passport 폴더에 index 파일
 
@@ -37,6 +38,7 @@ passportConfig();
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));   // /img 주소로 이미지요청시 /uploads 경로를 찾음
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -57,6 +59,7 @@ app.use(passport.session());
 
 app.use(`/`, pageRouter);
 app.use('/auth', authRouter);
+app.use('/post', postRouter);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
--- lecture/ch9/lecture/routes/page.js
+++ lecture/ch9/lecture/routes/page.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const {Post, User} = require('../models');
 const router = express.Router();
 
 router.use((req, res, next) => {
-    res.locals.user = null
+    res.locals.user = req.user;
     res.locals.followerCount = 0;
-    res.locals.followerCount = 0;
+    res.locals.followingCount = 0;
     res.locals.followerIdList = [];
     next();
 });
@@ -17,13 +18,23 @@ router.get('/join', (req, res) => {
     res.render('join', {title: '회원가입 - NodeBird'});
 });
 
-router.get('/', (req, res, next) => {
-    const twits = [];
-    res.render('main', {
-        title: 'NodeBird',
-        twits,
-        user: req.user,
-    });
+router.get('/', async (req, res, next) => {
+    try {
+        const posts = await Post.findAll({
+            include: {
+                model: User,
+                attributes: ['id', 'nick'], // 조인 테이블에서 가져올 컬럼
+            },
+            order: [['createdAt', 'DESC']],
+        });
+        res.render('main', {
+            title: 'NodeBird',
+            twits: posts,
+        })
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 });
 
 module.exports = router;
