d8ec946fce919e9ed7abdfe886a0b817a64bdc6b
sh6986 <okok960411@gmail.com>
2022-01-27 18:17:36 +0900

    해시태그 검색하기

--- lecture/ch9/lecture/routes/page.js
+++ lecture/ch9/lecture/routes/page.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {Post, User} = require('../models');
+const {Post, User, Hashtag} = require('../models');
 const router = express.Router();
 
 router.use((req, res, next) => {
@@ -36,5 +36,31 @@ router.get('/', async (req, res, next) => {
         next(err);
     }
 });
+
+// GET /hashtag?hashtag=노드
+router.get('/hashtag', async (req, res, next) => {
+    const query = decodeURIComponent(req.query.hashtag);
+
+    if (!query) {
+        return res.redirect('/');
+    }
+
+    try {
+        const hashtag = await Hashtag.findOne({where: {title: query}});
+        let posts = [];
+
+        if (hashtag) {
+            posts = await hashtag.getPosts({include: [{model: User, attributes: ['id', 'nick']}]});
+        }
+
+        return res.render('main', {
+            title: `#${query} | 검색 결과 | NodeBird`,
+            twits: posts,
+        });
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+})
 
 module.exports = router;
