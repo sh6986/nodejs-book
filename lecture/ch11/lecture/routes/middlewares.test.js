4eebce8f72c875e8d81605b7736ff666ad35a855
sh6986 <okok960411@gmail.com>
2022-01-31 10:23:36 +0900

    단위 테스트하기

--- lecture/ch11/lecture/routes/middlewares.test.js
+++ lecture/ch11/lecture/routes/middlewares.test.js
@@ -1,3 +1,53 @@
-test('1 + 1은 2입니다.', () => {
-    expect(1 + 1).toEqual(2);
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+
+describe('isLoggedIn', () => {
+    // 이런식으로 가짜로 값을 만들어두는것 -> 모킹
+    const res = {
+        status: jest.fn(() => res),
+        send: jest.fn(),
+    };
+    const next = jest.fn();
+    test('로그인되어있으면 isLoggedIn 이 next를 호출해야 함', () => {
+        const req = {
+            isAuthenticated: jest.fn(() => true),
+        };
+        isLoggedIn(req, res, next);
+        expect(next).toBeCalledTimes(1);
+    });
+
+    test('로그인되어있지 않으면 isLoggedIn 이 에러를 응답해야 함', () => {
+        const req = {
+            isAuthenticated: jest.fn(() => false),
+        };
+        isLoggedIn(req, res, next);
+        expect(res.status).toBeCalledWith(403);
+        expect(res.send).toBeCalledWith('로그인 필요');
+    });
 });
+
+describe('isNotLoggedIn', () => {
+    const res = {
+        status: jest.fn(() => res),
+        send: jest.fn(),
+        redirect: jest.fn(),
+    };
+    const next = jest.fn();
+    test('로그인되어있으면 isNotLoggedIn 이 에러를 응답해야 함', () => {
+        const req = {
+            isAuthenticated: jest.fn(() => true),
+        };
+        const message = encodeURIComponent('로그인한 상태입니다.');
+        isNotLoggedIn(req, res, next);
+        expect(res.redirect).toBeCalledWith(`/?error=${message}'`);
+    });
+
+    test('로그인되어있지 않으면 isNotLoggedIn 이 next를 호출해야 함', () => {
+        const req = {
+            isAuthenticated: jest.fn(() => false),
+        };
+        isNotLoggedIn(req, res, next);
+        expect(next).toBeCalledTimes(1);
+    });
+});
+
+
