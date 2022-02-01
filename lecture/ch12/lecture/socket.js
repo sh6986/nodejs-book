7dbed303f8a48edce0e5b1b67ff24c49b19aea9e
sh6986 <okok960411@gmail.com>
2022-02-01 16:48:31 +0900

    웹 소켓으로 실시간 데이터 전송하기 - Socket.IO 사용해보기

--- lecture/ch12/lecture/socket.js
+++ lecture/ch12/lecture/socket.js
@@ -1,30 +1,28 @@
-const WebSocket = require('ws');    // node에서 사용하는 웹소켓
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-    const wss = new WebSocket.Server({server}); // express 서버랑 웹소켓 서버랑 연결
+    const io = SocketIO(server, {path: '/socket.io'});  // express랑 socket.io 연결
+        // 연결하면 socket.io가 express에 <script src="/socket.io/socket.io.js"></script> 라고 작성한 해당 파일을 넣어줌
 
-    wss.on('connection', (ws, req) => { // 웹소켓 연결시(프론트에서 new WebSocket("ws://localhost:8005")할떄 실행됨)
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;  // ip 파악
-        // req.headers['x-forwarded-for'] : 프록시서버를 사용하는 경우 ip 가 변조될 수 있으므로 그걸 캐치하기 위해
-        console.log('새로운 클라이언트 접속', ip);
-
-        ws.on('message', (message) => { // 클라이언트로부터 메시지(클라이언트에서 webSocket.send()할때)
-            console.log(message);
+    io.on('connection', (socket) => {   // 웹소켓 연결 시
+        const req = socket.request;
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log('새로운 클라이언트 접속!', ip, socket.id, req.id);
+        
+        socket.on('disconnect', () => { // 연결 종료 시
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval);
         });
-
-        ws.on('error', (error) => {  // 에러 시
+        socket.on('error', (error) => { // 에러 시
             console.error(error);
         });
-
-        ws.on('close', () => {  // 연결 종료 시
-            console.log('클라이언트 접속 해제', ip);
-            clearInterval(ws.interval);
+        socket.on('reply', (data) => {  // 클라이언트로부터 메시지
+            console.log(data);
         });
-
-        ws.interval = setInterval(() => { // 3초마다 클라이언트로 메시지 전송
-            if (ws.readyState === ws.OPEN) {    // 웹소켓이 클라이언트랑 서버랑 연결되어있을때
-                ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-            }
+        socket.interval = setInterval(() => {   // 3초마다 클라이언트로 메시지 전송
+            socket.emit('news', 'Hello Socket.ID');
         }, 3000);
     });
 };
+
+
