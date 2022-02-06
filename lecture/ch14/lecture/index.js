db707961672bf255679502089aa3c259332b7c18
sh6986 <okok960411@gmail.com>
2022-02-06 17:49:03 +0900

    CLI 프로그램 만들기 - readline 사용하기

--- lecture/ch14/lecture/index.js
+++ lecture/ch14/lecture/index.js
@@ -1,2 +1,26 @@
 #!/usr/bin/env node
-console.log('Hello CLI', process.argv);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.clear();
+
+const answerCallback = (answer) => {
+    if (answer === 'y') {
+        console.log('감사');
+        rl.close();
+    } else if (answer === 'n') {
+        console.log('죄송');
+        rl.close();
+    } else {
+        console.clear();
+        console.log('y나 n만 입력하세요.');
+        rl.question('예제가 재미있습니까? (y/n)', answerCallback);
+    }
+    rl.close();
+};
+
+rl.question('예제가 재미있습니까? (y/n)', answerCallback);
